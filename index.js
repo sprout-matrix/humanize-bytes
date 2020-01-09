6c52d77ee424b9a6bf5c2f73001f2e2a73416991
Hushen Savani <hushen.savani@sproutmatrix.com>
2020-01-09 22:55:08 +0530

    Updated options

--- index.js
+++ index.js
@@ -3,19 +3,19 @@
 *  @author sprout-matrix
 *  Converts bytes to human readable data units, such as KB, MB, GB, TB, PB, EB, ZB, YB etc.  
 **/
-function bytesToHumanReadableFormat(bytes, multiplier) {
+function bytesToHumanReadableFormat(bytes, options = { multiplier: 1024 }) {
 	if(typeof bytes !== 'number')
 		bytes = 1;
 
-	if(!multiplier || typeof multiplier !== 'number')
-		multiplier = 1024;
+	if(typeof options.multiplier !== 'number')
+		options['multiplier'] = 1024;
 
 	bytes = Math.abs(bytes);
 
-    var index = Math.floor(Math.log(bytes) / Math.log(1024)),
+    var index = Math.floor(Math.log(bytes) / Math.log(options.multiplier)),
     units = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
 
-    return (bytes / Math.pow(multiplier, index)).toFixed(2) * 1 + ' ' + units[index];
+    return (bytes / Math.pow(options.multiplier, index)).toFixed(2) * 1 + ' ' + units[index];
 }
 
 module.exports = bytesToHumanReadableFormat;
